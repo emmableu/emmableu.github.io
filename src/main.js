3376bd7a0ad4752c6cbe0ae84749da74076c2ff1
wwang33 <wwang33@ncsu.edu>
2021-02-01 19:19:39 -0800

    added vue-analytics

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueAnalytics from 'vue-analytics';
+//and then use it in main.js
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
+Vue.use(VueAnalytics, {
+  id: 'G-N4ZB3T4TCC'
+});
